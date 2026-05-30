5dd97dc007da5fa681193e4488a7933ffc6b8740
yoursluo <877479978@qq.com>
2026-05-30 15:00:15 +0800

    chore: bump version to 1.3.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.photodiary"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.3.1"
     }
 
     buildTypes {
